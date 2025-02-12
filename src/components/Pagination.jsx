0bee3b15ac169609344b3ee16ebe267b3652872f
MogesBekele <mogesbekele32@gmail.com>
2025-02-12 21:14:16 +0300

    changed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,21 +4,26 @@ import { useState } from 'react';
 
 const Pagination = () => {
   const [currentPage, setCurrentPage] = useState(1);
-
+  const [jobsPerPage, setJobsPerPage] = useState(5);
+  const indexOfLastJob = currentPage * jobsPerPage;
+  const indexOfFirstJob = indexOfLastJob - jobsPerPage;
+  const currentJobs = jobsData.slice(indexOfFirstJob, indexOfLastJob);
   return (
 
     <div>
 
       {
-        jobsData.map((job, index)=>(
+         currentJobs.map((job, index)=>(
           <div className=' gap-4 ' key={index}>
             <h1>{job.title}</h1>
             <h2>{job.level}</h2>
             <h3>{job.location}</h3>
             <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
           </div>
+
         ))
       }
+      <button>{currentJobs.map(())}</button>
       
     </div>
   )
